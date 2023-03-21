c1a78f1a1a372cd6b9fb09335d97261dc67f869d
Samuael Ketema <lijsamuael@gmail.com>
2023-03-21 08:41:56 +0300

    (fix) making the max width of the order page to 1500px

--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -3,7 +3,7 @@ import OrderNavBar from "@/components/order/navbar";
 
 export default function OrderLayout({ children }: { children: any }) {
   return (
-    <div>
+    <div className="max-w-[1500px] mx-auto">
       <OrderNavBar />
       {children}
       <OrderFooter />
